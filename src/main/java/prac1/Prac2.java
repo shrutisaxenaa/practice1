94e991c8e746a96b3faddbf59cc6e9e137c5c282
shrutisaxenaa <106474924+shrutisaxenaa@users.noreply.github.com>
2022-07-07 14:21:39 -0500

    commit

--- src/main/java/prac1/Prac2.java
+++ src/main/java/prac1/Prac2.java
@@ -8,6 +8,13 @@ public class Prac2 {
 		int b=4;
 		int sum= a+b;
 		System.out.println(sum);
+		
+		System.out.println(product(2,3));
+	}
+	
+	public static int product(int a,int b) {
+		return a*b;
+		
 	}
 
 }
