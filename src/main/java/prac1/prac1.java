59b60d6d09fb7df8560625a8e9bd95b2ad9b58c4
shrutisaxenaa <106474924+shrutisaxenaa@users.noreply.github.com>
2022-07-07 08:03:03 -0500

    commit

--- src/main/java/prac1/prac1.java
+++ src/main/java/prac1/prac1.java
@@ -5,6 +5,11 @@ public class prac1 {
 	public static void main(String[] args) {
 		
 		System.out.println("hello, this is Shruti");
+		
+		words();
+	}
+	public static void words() {
+		System.out.println("namastey");
 	}
 
 }
